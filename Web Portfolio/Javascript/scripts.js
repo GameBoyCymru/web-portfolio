bf0897709111bd0779e1c50a5d5e39ae787b1d09
Josh.T <115492240+joshuathomas22@users.noreply.github.com>
2023-12-09 14:01:40 +0000

    Adds a navigation bar

--- Web Portfolio/Javascript/scripts.js
+++ Web Portfolio/Javascript/scripts.js
@@ -1,3 +1,7 @@
+/*
+------------------------------------------------------------
+*/
+
 // Typing animation
 const texts = ["Teaching Assistant.", "Childcare Assistant.", "C++ Programmer.", "Gamer."];
 let textIndex = 0;
@@ -31,45 +35,117 @@ function erase() {
 // Start the typing animation
 type();
 
+/*
+------------------------------------------------------------
+*/
+
+//  Makes the first two letters of each word bold
+
+// let isBold = false;   // Sets the default state to false
+
+// // Bold the first two letters of each paragraph
+// function toggleBoldText() {
+//   // Get all the paragraph elements on the page
+//   const paragraphs = document.querySelectorAll('p');
+
+//   // Iterate through each paragraph element
+//   paragraphs.forEach(paragraph => {
+//     // Split the text into words
+//     const words = paragraph.innerText.split(' ');
+
+//     // Iterate through each word and toggle bold styling
+//     const formattedWords = words.map(word => {
+//       if (word.length >= 2) {
+//         const boldText = isBold ? word : `<b>${word.substring(0, 2)}</b>${word.substring(2)}`;
+//         return boldText;
+//       }
+//       return word;
+//     });
+
+//     // Join the formatted words back into a string
+//     const formattedText = formattedWords.join(' ');
+
+//     // Set the inner HTML of the paragraph with the formatted text
+//     paragraph.innerHTML = formattedText;
+//   });
+
+//   // Toggle the state
+//   isBold = !isBold;
+
+//   // Update the button text
+//   const buttonText = isBold ? 'Revert to Normal' : 'Make Bold';
+//   document.getElementById('boldButton').innerText = buttonText;
+// }
 
 
-let isBold = false;   // Sets the default state to false
+// let boldButton = document.getElementById('boldTextToggle');
 
-// Bold the first two letters of each paragraph
-function toggleBoldText() {
-  // Get all the paragraph elements on the page
-  const paragraphs = document.querySelectorAll('p');
+// boldButton.addEventListener('click', boldTextToggle);
 
-  // Iterate through each paragraph element
-  paragraphs.forEach(paragraph => {
-    // Split the text into words
-    const words = paragraph.innerText.split(' ');
+/*
+------------------------------------------------------------
+*/
 
-    // Iterate through each word and toggle bold styling
-    const formattedWords = words.map(word => {
-      if (word.length >= 2) {
-        const boldText = isBold ? word : `<b>${word.substring(0, 2)}</b>${word.substring(2)}`;
-        return boldText;
-      }
-      return word;
+// Page Navigation
+
+// Open and close the quick settings menu
+const settingsButton = document.querySelector('.settings');
+const closeSettings = document.querySelector('.settings-close');
+const quickSettings = document.querySelector('.quick-settings');
+
+settingsButton.addEventListener('click', () => {
+    quickSettings.classList.remove('none');
+    quickSettings.classList.remove('hide');
+});
+
+closeSettings.addEventListener('click', () => {
+    quickSettings.classList.add('hide');
+});
+
+
+
+// Open and close the navigation menu
+const navButton = document.querySelector('.nav-btn');
+const closeNav = document.querySelector('.nav-close');
+const navSettings = document.querySelector('.quick-nav');
+
+navButton.addEventListener('click', () => {
+    navSettings.classList.remove('none');
+    navSettings.classList.remove('hide');
+});
+
+closeNav.addEventListener('click', () => {
+    navSettings.classList.add('hide');
+});
+
+
+
+
+// Get all sections and screen title
+const sections = document.querySelectorAll('.container');
+const screenTitle = document.querySelector('.screen-title');
+
+// Function to update screen title based on scroll position
+function updateScreenTitle() {
+    let closestSection = sections[0]; // Initialize with the first section
+
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        const currentClosestRect = closestSection.getBoundingClientRect();
+
+        // Check if the current section is closer to the top of the viewport
+        if (Math.abs(rect.top) < Math.abs(currentClosestRect.top)) {
+            closestSection = section;
+        }
     });
 
-    // Join the formatted words back into a string
-    const formattedText = formattedWords.join(' ');
-
-    // Set the inner HTML of the paragraph with the formatted text
-    paragraph.innerHTML = formattedText;
-  });
-
-  // Toggle the state
-  isBold = !isBold;
-
-  // Update the button text
-  const buttonText = isBold ? 'Revert to Normal' : 'Make Bold';
-  document.getElementById('boldButton').innerText = buttonText;
+    // Update the screen title with the closest section's title
+    screenTitle.textContent = closestSection.querySelector('h1').textContent;
 }
 
+// Listen for scroll events and update screen title
+document.addEventListener('scroll', updateScreenTitle);
 
-let boldButton = document.getElementById('boldButton');
-
-boldButton.addEventListener('click', toggleBoldText);
+/*
+------------------------------------------------------------
+*/
