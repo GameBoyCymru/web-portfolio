c091c4ffe5a039a8a6ab00f59a6a51cc466e536f
Josh.T <115492240+joshuathomas22@users.noreply.github.com>
2023-12-10 10:48:47 +0000

    Most quick settings functions now work.

--- Web Portfolio/Javascript/scripts.js
+++ Web Portfolio/Javascript/scripts.js
@@ -39,48 +39,46 @@ type();
 ------------------------------------------------------------
 */
 
-//  Makes the first two letters of each word bold
-
-// let isBold = false;   // Sets the default state to false
-
-// // Bold the first two letters of each paragraph
-// function toggleBoldText() {
-//   // Get all the paragraph elements on the page
-//   const paragraphs = document.querySelectorAll('p');
-
-//   // Iterate through each paragraph element
-//   paragraphs.forEach(paragraph => {
-//     // Split the text into words
-//     const words = paragraph.innerText.split(' ');
-
-//     // Iterate through each word and toggle bold styling
-//     const formattedWords = words.map(word => {
-//       if (word.length >= 2) {
-//         const boldText = isBold ? word : `<b>${word.substring(0, 2)}</b>${word.substring(2)}`;
-//         return boldText;
-//       }
-//       return word;
-//     });
-
-//     // Join the formatted words back into a string
-//     const formattedText = formattedWords.join(' ');
-
-//     // Set the inner HTML of the paragraph with the formatted text
-//     paragraph.innerHTML = formattedText;
-//   });
-
-//   // Toggle the state
-//   isBold = !isBold;
-
-//   // Update the button text
-//   const buttonText = isBold ? 'Revert to Normal' : 'Make Bold';
-//   document.getElementById('boldButton').innerText = buttonText;
-// }
+// Bold the first two letters of each paragraph
 
 
-// let boldButton = document.getElementById('boldTextToggle');
+let isBold = false; // Sets the default state to false
 
-// boldButton.addEventListener('click', boldTextToggle);
+// Bold the first two letters of each paragraph
+function toggleBoldText() {
+    // Get all the paragraph elements on the page
+    const paragraphs = document.querySelectorAll('p');
+
+    // Iterate through each paragraph element
+    paragraphs.forEach(paragraph => {
+        // Split the text into words
+        const words = paragraph.innerText.split(' ');
+
+        // Iterate through each word and toggle bold styling
+        const formattedWords = words.map(word => {
+            if (word.length >= 2) {
+                const boldText = isBold ? word : `<b>${word.substring(0, 2)}</b>${word.substring(2)}`;
+                return boldText;
+            }
+            return word;
+        });
+
+        // Join the formatted words back into a string
+        const formattedText = formattedWords.join(' ');
+
+        // Set the inner HTML of the paragraph with the formatted text
+        paragraph.innerHTML = formattedText;
+    });
+
+    // Toggle the state
+    isBold = !isBold;
+}
+
+// Get the checkbox for bold text
+const boldTextCheckbox = document.getElementById('boldTextToggle');
+
+// Add an event listener to the checkbox
+boldTextCheckbox.addEventListener('change', toggleBoldText);
 
 /*
 ------------------------------------------------------------
@@ -160,10 +158,65 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
-
-
 /*
 ------------------------------------------------------------
 */
+
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  // Get the checkbox for dark mode
+  const darkModeCheckbox = document.getElementById('darkModeToggle');
+
+  // Apply dark mode if the checkbox is checked
+  if (darkModeCheckbox.checked) {
+      document.documentElement.style.setProperty('--primary', '#a39be3');
+      document.documentElement.style.setProperty('--secondary', '#7a2268');
+      document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 0.8)');
+      document.documentElement.style.setProperty('--background-image', 'url("./Backgrounds/island-dark.png")');
+      document.documentElement.style.setProperty('--primary-text', '#e7e6f7');
+      document.documentElement.style.setProperty('--secondary-text', '#131B23');
+      document.documentElement.style.setProperty('--nav-background', 'rgb(6 5 20 / 95%)');
+      document.documentElement.style.setProperty('--nav-text', '#e7e6f7');
+      document.documentElement.style.setProperty('--exit-filter', 'invert(100%) sepia(0%) saturate(1513%) hue-rotate(233deg) brightness(107%) contrast(101%)');
+
+
+  } else {
+      // Apply light mode if the checkbox is unchecked
+      document.documentElement.style.setProperty('--primary', '#8fffbc');
+      document.documentElement.style.setProperty('--secondary', '#7a2268');
+      document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 0.8)');
+      document.documentElement.style.setProperty('--background-image', 'url("./Backgrounds/island-light.png")');
+      document.documentElement.style.setProperty('--primary-text', '#e7e6f7');
+      document.documentElement.style.setProperty('--secondary-text', '#131B23');
+      document.documentElement.style.setProperty('--nav-background', 'rgb(234 233 253 / 80%)');
+      document.documentElement.style.setProperty('--nav-text', '#131B23');
+      document.documentElement.style.setProperty('--exit-filter', 'invert(8%) sepia(14%) saturate(1423%) hue-rotate(169deg) brightness(94%) contrast(94%)');
+    }
+
+  // Save the user's preference in local storage
+  localStorage.setItem('darkModePreference', darkModeCheckbox.checked);
+}
+
+// Get the checkbox for dark mode
+const darkModeCheckbox = document.getElementById('darkModeToggle');
+
+// Add an event listener to the checkbox
+darkModeCheckbox.addEventListener('change', toggleDarkMode);
+
+// Retrieve the user's preference from local storage (if available)
+const savedDarkModePreference = localStorage.getItem('darkModePreference');
+
+// If a preference is saved, apply it
+if (savedDarkModePreference !== null) {
+  darkModeCheckbox.checked = savedDarkModePreference === 'true';
+  toggleDarkMode(); // Update colors based on the saved preference
+} else {
+  // If no preference is saved, use the system preference
+  darkModeCheckbox.checked = window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+// Initial call to toggleDarkMode to set initial color scheme
+toggleDarkMode();
+
+
