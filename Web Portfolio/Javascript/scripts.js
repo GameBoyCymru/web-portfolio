e314aa66c51e4d4cbf9c20cf5c65953a0589b645
Josh.T <115492240+joshuathomas22@users.noreply.github.com>
2023-12-19 12:11:26 +0000

    Solid colours now saves when switching themes.

--- Web Portfolio/Javascript/scripts.js
+++ Web Portfolio/Javascript/scripts.js
@@ -190,6 +190,8 @@ document.addEventListener("DOMContentLoaded", function () {
 function toggleDarkMode() {
   // Get the checkbox for dark mode
   const darkModeCheckbox = document.getElementById('darkModeToggle');
+  // check if solid colours is toggled on or off
+  const solidColoursCheckbox = document.getElementById('solidColoursToggle');
 
   // Determine the theme based on the state of the toggle
   const theme = darkModeCheckbox.checked ? 'dark' : 'light';
@@ -205,26 +207,42 @@ function toggleDarkMode() {
   if (darkModeCheckbox.checked) {
     document.documentElement.style.setProperty('--primary', '#a39be3');
     document.documentElement.style.setProperty('--secondary', '#7a2268');
-    document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 0.8)');
     document.documentElement.style.setProperty('--background-image', 'url("./Backgrounds/island-dark.webp")');
     document.documentElement.style.setProperty('--primary-text', '#e7e6f7');
     document.documentElement.style.setProperty('--secondary-text', '#131B23');
-    document.documentElement.style.setProperty('--nav-background', 'rgb(6 5 20 / 0.8)');
     document.documentElement.style.setProperty('--nav-text', '#e7e6f7');
     document.documentElement.style.setProperty('--exit-filter', 'invert(100%) sepia(0%) saturate(1513%) hue-rotate(233deg) brightness(107%) contrast(101%)');
+
+    if (solidColoursCheckbox.checked) {
+      document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 1)');
+      document.documentElement.style.setProperty('--nav-background', 'rgb(6 5 20 / 1)');
+    }
+    else {
+      document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 0.8)');
+      document.documentElement.style.setProperty('--nav-background', 'rgb(6 5 20 / 0.8)');
+    }
 
 
   } else {
     // Apply light mode if the checkbox is unchecked
     document.documentElement.style.setProperty('--primary', '#8fffbc');
     document.documentElement.style.setProperty('--secondary', '#a39be3');
-    document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 0.8)');
     document.documentElement.style.setProperty('--background-image', 'url("./Backgrounds/island-light.webp")');
     document.documentElement.style.setProperty('--primary-text', '#e7e6f7');
     document.documentElement.style.setProperty('--secondary-text', '#131B23');
-    document.documentElement.style.setProperty('--nav-background', 'rgb(234 233 253 / 0.8)');
     document.documentElement.style.setProperty('--nav-text', '#131B23');
     document.documentElement.style.setProperty('--exit-filter', 'invert(8%) sepia(14%) saturate(1423%) hue-rotate(169deg) brightness(94%) contrast(94%)');
+
+    if (solidColoursCheckbox.checked) {
+      document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 1)');
+      document.documentElement.style.setProperty('--nav-background', 'rgb(234 233 253 / 1)');
+    }
+
+    else {
+      document.documentElement.style.setProperty('--background', 'rgba(6, 5, 20, 0.8)');
+      document.documentElement.style.setProperty('--nav-background', 'rgb(234 233 253 / 0.8)');
+    }
+
   }
 
   // Save the user's preference in local storage
@@ -268,39 +286,23 @@ function toggleSolidColours() {
   // check if dark mode is toggled on or off
   const darkModeCheckbox = document.getElementById('darkModeToggle');
 
-  let backgroundColor = solidColoursCheckbox.checked;
-  let navBackgroundColor = solidColoursCheckbox.checked;
+  // Determine the background color based on the state of the toggle
+  const backgroundColor = solidColoursCheckbox.checked
+    ? darkModeCheckbox.checked  // Solid color for dark mode background elements
+      ? 'rgba(6, 5, 20, 1)'
+      : 'rgba(6, 5, 20, 1)'
+    : darkModeCheckbox.checked  // Transparent color for dark mode background elements
+      ? 'rgba(6, 5, 20, 0.8)'
+      : 'rgba(6, 5, 20, 0.8)';
 
-
-  if (darkModeCheckbox.checked) {
-    // Determine the background color based on the state of the toggle
-    backgroundColor = solidColoursCheckbox.checked
-      ? 'rgba(6, 5, 20, 1)' // Solid color for background elements
-      : 'rgba(6, 5, 20, 0.8)' // Transparent color for background elements
-
-    // Determine the navigation background color based on the state of the toggle
-    navBackgroundColor = solidColoursCheckbox.checked
-      ? 'rgba(6, 5, 20, 1)' // Solid color for background elements
-      : 'rgba(6, 5, 20, 0.8)' // Transparent color for background elements
-
-  }
-  else {
-
-    // Determine the background color based on the state of the toggle
-    backgroundColor = solidColoursCheckbox.checked
-      ? 'rgba(6, 5, 20, 1)' // Solid color for background elements
-      : 'rgba(6, 5, 20, 0.8)' // Transparent color for background elements
-
-
-    // Determine the navigation background color based on the state of the toggle
-    navBackgroundColor = solidColoursCheckbox.checked
-      ? 'rgb(234 233 253 / 1)' // Solid color for background elements
-      : 'rgb(234 233 253 / 0.8)' // Transparent color for background elements
-  }
-
-
-
-
+  // Determine the navigation background color based on the state of the toggle
+  const navBackgroundColor = solidColoursCheckbox.checked
+    ? darkModeCheckbox.checked
+      ? 'rgba(6, 5, 20, 1)' // Solid color for dark mode background elements
+      : 'rgba(234, 233, 253, 1)' // Solid color for light mode background elements
+    : darkModeCheckbox.checked
+      ? 'rgba(6, 5, 20, 0.8)' // Transparent color for dark mode background elements
+      : 'rgba(234, 233, 253, 0.8)'; // Transparent color for light mode background elements
 
 
   // Update the background color of body
@@ -333,4 +335,3 @@ if (savedSolidColoursPreference !== null) {
   solidColoursCheckbox.checked = false;
   toggleSolidColours();
 }
-
